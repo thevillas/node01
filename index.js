f2b250da354d68c746f1d9698ec84df8029fa9cf
thevillas <villalbas991@gmail.com>
2023-07-27 17:26:03 -0500

    terminado

--- index.js
+++ index.js
@@ -3,9 +3,33 @@ const app = express()
 const port = 5000;
 const morgan = require('')
 app.use(morgan('dev'));
+app.use(express.json());
+
+const service = require('./src/servicio.js')
 
 
 
+app.get("/", (req, res) => {
+    res.json({
+        mensage: "lista de usuarios",
+        body: service.getUser(),
+})
+
+});
+
+app.get('/:id', (req, res) => {
+    const id = req.params.id;
+})
+
+
+app.post("/", (req, res) => {
+    let { body : newUser} = req
+    let user = service.createUser(newUser);
+    res.status(201).json({
+        mensage : "nuevo usuario creado",
+        body: user
+    });
+})
 
 
 app: listen(PORT,()=>('server run on $(port)'))
